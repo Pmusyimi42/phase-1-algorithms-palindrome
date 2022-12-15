06d0eb4b73d4989f310b506d47b58272373c018c
patrick kisyula <pmusyimi42@gmail.com>
2022-12-15 12:02:23 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const lengthOfString = word.length;
+  for(let i = 0; i < lengthOfString/2 ; i++) {
+    if(word[i] === word[lengthOfString-1-i]){
+      return true;
+    }
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  find the length of a string
+  loop through half of the string
+  check if first and last characters of a string are the same.
+  if the first and  last characters of a string are the same
+  return true
+  if the first and last characters of a string are not the same
+  return false
 */
 
 /*
@@ -20,6 +34,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("eye"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ear"));
 }
 
 module.exports = isPalindrome;
